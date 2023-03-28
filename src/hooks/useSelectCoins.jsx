8a0a15e3bc8054c3145bafc789fad693c30c1c34
fribas84 <fribas84.gmail.com>
2023-03-28 15:12:49 -0300

    custom Hook with state, ext data file

--- src/hooks/useSelectCoins.jsx
+++ src/hooks/useSelectCoins.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import {useState} from 'react'
 import tw from "tailwind-styled-components";
 
 const Label = tw.label`
     font-bold
     mt-2
     text-white
-    flex
-    center
+    block
+    justify-self-auto
+    text-lg 
+`
+const Select = tw.select`
+    block
+    m-1
+    w-full
+    p-1
+    rounded-md
+
 `
 
-const useSelectCoins = (label) => {
+const useSelectCoins = (label,options) => {
+
+    const [state,setState] = useState('');
 
     const SelectCoins = () =>{
 
-        return (<Label>{label}</Label>)
-        
+        return(
+        <>
+        <Label>{label}</Label>
+        <Select 
+            value={state}
+            onChange={e=>setState(e.target.value)}
+            >
+            <option value=''>Select</option>
+            {
+                options?.map(opt=>(
+
+                    <option
+                        key={opt.id}
+                        value={opt.id}
+                        >{opt.name}</option>
+                ))}
+
+        </Select>
+        </>
+        )
     }
 
     return [SelectCoins]
