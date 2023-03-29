5155bded885ecf0c677b18e6764477899963f96a
fribas84 <fribas84.gmail.com>
2023-03-29 17:49:51 -0300

    added api call to retrieve 10 top coins

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import tw from "tailwind-styled-components";
 import useSelectCoins from '../hooks/useSelectCoins';
 import { fiatCoins } from '../data/fiatCoins'; 
@@ -22,12 +22,35 @@ const InputSubmit = tw.input`
 
 const Form = () => {
 
+    const [cryptos, setCryptos] = useState([]);
     const [fiatCoin, SelectFiatCoins] = useSelectCoins('Select your Fiat Coin',fiatCoins);
-    const [crypto, SelectCryptoCoins] = useSelectCoins('Select your Crypto');
+    const [crypto, SelectCryptoCoins] = useSelectCoins('Select your Crypto',cryptos);
 
+    useEffect(()=>{
+        const getData = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const res = await fetch(url);
+            const resData  = await res.json();
+            const arrayData = resData.Data.map(cr =>{
+                const obj = {
+                    id: cr.CoinInfo.Name,
+                    name: cr.CoinInfo.FullName
+                }
+                return obj;
+            });
+            setCryptos(arrayData);          
+        }
+        getData();
+    },[])
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log('en handleSubmit');
+    }
     
   return (
-    <form>
+    <form
+        onSubmit={handleSubmit}>
         <SelectFiatCoins />
         <SelectCryptoCoins />
         <InputSubmit  type='submit' value='Check Price' />
